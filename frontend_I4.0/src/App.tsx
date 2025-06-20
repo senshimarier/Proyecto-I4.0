55838b6a33a7f90afd488539b089b51104e9d70a
Maria Fernanda Guerrero Calderón <169078581+senshimarier@users.noreply.github.com>
2025-06-20 11:34:39 -0600

    Avances 20-06-25

--- frontend_I4.0/src/App.tsx
+++ frontend_I4.0/src/App.tsx
@@ -1,10 +1,39 @@
+// src/App.tsx
+
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// 1. Importa el Dashboard y TODAS las páginas que irán dentro de él
+import Dashboard from './modules/Dashboard';
+import HomePage from './modules/home/HomePage';
 import UserForm from './modules/user/User_Form';
+import ProductsPage from './modules/product/ProductsPage';
+import OrdersPage from './modules/order/OrdersPage';
 
 function App() {
   return (
-    <div>
-      <UserForm />
-    </div>
+    <Router>
+      <Routes>
+        {/*
+        rutas de dashboard
+        */}
+        <Route path="/" element={<Dashboard />}>
+          
+          {/* rutas anidadas */}
+          <Route index element={<HomePage />} />
+          {/*rutas padre */}
+
+          <Route path="users" element={<UserForm />} />
+          {/*users */}
+
+          <Route path="products" element={<ProductsPage />} />
+          <Route path="orders" element={<OrdersPage />} />
+        </Route>
+
+        {/* siguientes rutas */}
+
+      </Routes>
+    </Router>
   );
 }
 
